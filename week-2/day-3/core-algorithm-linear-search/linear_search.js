d3477ab887f995c24b0705c8cb0189dc59dce454
Denis Ignatenko <luuffyakka@yandex.ru>
2023-10-19 12:48:49 +0300

    Linear search + tests

--- week-2/day-3/core-algorithm-linear-search/linear_search.js
+++ week-2/day-3/core-algorithm-linear-search/linear_search.js
@@ -1,14 +1,29 @@
 //Реализуй функции линейного поиска. Проверь результат тестами.
 
-function linearSearch() {
-
+function linearSearch(char, arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === char) {
+      return i;
+    }
+  }
+  return -1;
 }
 
-function globalLinearSearch() {
+function globalLinearSearch(char, arr) {
+  let indexes = [];
+  let found = false;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === char) {
+      indexes.push(i);
+      found = true;
+    }
+  }
+
+  return found ? indexes : -1;
 }
 
 module.exports = {
-    linearSearch,
-    globalLinearSearch
-}
+  linearSearch,
+  globalLinearSearch,
+};
--- week-2/day-3/core-algorithm-linear-search/spec/linear_search.spec.js
+++ week-2/day-3/core-algorithm-linear-search/spec/linear_search.spec.js
@@ -1,17 +1,53 @@
-const { linearSearch, globalLinearSearch } = require('../linear_search');
+const { linearSearch, globalLinearSearch } = require("../linear_search");
 
-describe("linearSearch()", function() {
-    it("returns first index of item in array", function() {
-        let item = 3;
-        let arr = [1,2,3,4,5];
-        expect(linearSearch(item, arr)).toEqual(2);
-    });
+describe("linearSearch()", function () {
+  it("возвращает индекс искомого значения", function () {
+    let item = 3;
+    let arr = [1, 2, 3, 4, 5];
+    expect(linearSearch(item, arr)).toEqual(2);
+  });
+
+  it("возвращает -1 если элемента нет в массиве", function () {
+    let item = 6;
+    let arr = [1, 2, 3, 4, 5];
+    expect(linearSearch(item, arr)).toEqual(-1);
+  });
+
+  it("возвращает -1 для пустого массива", function () {
+    let item = 3;
+    let arr = [];
+    expect(linearSearch(item, arr)).toEqual(-1);
+  });
+
+  it("возвращает первый индекс искомого значения если оно встречается несколько раз", function () {
+    let item = 3;
+    let arr = [1, 2, 3, 4, 5, 3, 6, 3];
+    expect(linearSearch(item, arr)).toEqual(2);
+  });
 });
 
-describe("globalLinearSearch()", function() {
-    it("returns array indxes of items in array", function() {
-        let item = 'a';
-        let arr = ['b', 'a', 'n', 'a', 'n', 'a', 's'];
-        expect(globalLinearSearch(item, arr)).toEqual([1, 3, 5]);
-    });
+describe("globalLinearSearch()", function () {
+  it("возвращает массив всех индексов искомого значения", function () {
+    let item = "a";
+    let arr = ["b", "a", "n", "a", "n", "a", "s"];
+    expect(globalLinearSearch(item, arr)).toEqual([1, 3, 5]);
+  });
+
+  it("возвращает массив с одним индексом, если символ встречается только один раз в массиве", function () {
+    let item = "e";
+    let arr = ["h", "e", "l", "l", "o"];
+    expect(globalLinearSearch(item, arr)).toEqual([1]);
+  });
+
+  it("возвращает пустой массив, если массив не задан", function () {
+    let item = "y";
+    let arr = [];
+    expect(globalLinearSearch(item, arr)).toEqual(-1);
+  });
+
+  it("возвращает -1, если символ не найден в массиве", function () {
+    let item = "y";
+    let arr = ["b", "a", "n", "a", "n", "a", "s"];
+    expect(globalLinearSearch(item, arr)).toEqual(-1);
+  });
 });
