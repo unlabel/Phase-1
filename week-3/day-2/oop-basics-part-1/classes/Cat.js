e3de785f70cbea55947a07ec9d10c7d364822a58
Denis Ignatenko <luuffyakka@yandex.ru>
2023-10-24 12:18:04 +0300

    Release 2

--- week-3/day-2/oop-basics-part-1/classes/Cat.js
+++ week-3/day-2/oop-basics-part-1/classes/Cat.js
@@ -1,4 +1,15 @@
-import {Mammal} from "./Mammal.js";
+import { Mammal } from "./Mammal.js";
 
 export class Cat extends Mammal {
+  constructor(name, breed) {
+    super(name);
+    this.breed = breed;
+  }
+  showInfo() {
+    return `Порода: ${this.breed}. Имя: ${this.name}`;
+  }
 }
+
+let myCat = new Cat("Varya", "Aegean");
+
+console.log(myCat.showInfo());
