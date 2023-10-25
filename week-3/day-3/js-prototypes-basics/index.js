662b508592b0feb8faa1b9a2c37aa312bc1c0152
Denis Ignatenko <luuffyakka@yandex.ru>
2023-10-25 14:34:25 +0300

    Release 0-2

--- week-3/day-3/js-prototypes-basics/index.js
+++ week-3/day-3/js-prototypes-basics/index.js
@@ -1,1 +1,55 @@
+function Person(name, age, gender) {
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
 
+  this.greeting = function () {
+    return `Привет, меня зовут ${this.name} и мне ${this.age} лет`;
+  };
+}
+
+let person = new Person("John", 25, "male");
+// console.log(person.greeting());
+
+function Animal(name, species) {
+  this.name = name;
+  this.species = species;
+
+  this.makeSound = function () {
+    return `This is ${this.name}. He's a ${this.species}.`;
+  };
+}
+
+function Dog(name, species) {
+  Animal.call(this, name, species);
+
+  this.fetch = function () {
+    return `Собака ${name} принесла палку`;
+  };
+}
+
+let dog = new Dog("Rex", "bulldog");
+// console.log(dog.fetch());
+// console.log(dog.makeSound());
+
+function Vehicle(make, model) {
+  this.make = make;
+  this.model = model;
+
+  this.start = function () {
+    return `Двигатель запущен`;
+  };
+}
+
+function Car(make, model) {
+  Vehicle.call(this, make, model);
+
+  this.drive = function () {
+    return `Машина ${this.make} ${this.model} поехала`;
+  };
+}
+
+let car = new Car("Toyota", "Camry");
+
+console.log(car.start());
+console.log(car.drive());
