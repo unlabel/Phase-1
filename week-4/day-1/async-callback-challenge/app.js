cbb0d0149c117dfa4a82d07f7134090b759ccd6d
Denis Ignatenko <luuffyakka@yandex.ru>
2023-10-30 12:26:14 +0300

    Release 0

--- week-4/day-1/async-callback-challenge/app.js
+++ week-4/day-1/async-callback-challenge/app.js
@@ -1,4 +1,27 @@
-function readFileAndProcess(readPath, writePath, callback) {}
+const fs = require("fs");
+
+function readFileAndProcess(readPath, writePath, callback) {
+  fs.readFile(readPath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Произошла ошибка при чтении файла:", err);
+      return;
+    }
+
+    const dataArray = data.split(" ");
+
+    callback(dataArray);
+
+    const result = dataArray.join(" ");
+
+    fs.writeFile(writePath, result, (err) => {
+      if (err) {
+        console.error("Произошла ошибка при записи файла:", err);
+        return;
+      }
+      console.log(`Результат записан в файл ${writePath}`);
+    });
+  });
+}
 
 function getUniqueWords(words) {}
 
