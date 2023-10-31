9995afd44c9cc7f8c253f8f543901d18c4c9204a
Denis Ignatenko <luuffyakka@yandex.ru>
2023-10-31 10:47:25 +0300

    Update

--- week-4/day-2/fs-files-progressbar/progressbarSync.js
+++ week-4/day-2/fs-files-progressbar/progressbarSync.js
@@ -1,19 +1,16 @@
 const fs = require("fs");
-// Раскомментируй только для релиза 4.
-// const runInteractiveConsole = require('./interactive');
-// runInteractiveConsole();
 
 // Синхронная загрузка одного файла
-function loadFile(filePath) {
+function loadFile(filePath, totalSize) {
   try {
     const data = fs.readFileSync(filePath, "utf8");
     const fileSize = Buffer.byteLength(data, "utf8");
     const delay = Math.floor(fileSize / 250);
     sleep(delay); // Имитация задержки загрузки
-    return data;
+    return fileSize;
   } catch (err) {
     console.error("Произошла ошибка при чтении файла:", err);
-    return null;
+    return 0;
   }
 }
 
@@ -29,16 +26,48 @@ function sleep(ms) {
 function loadFiles() {
   try {
     const files = fs.readdirSync("files");
+    let totalSize = 0; // Общий размер всех файлов
+
+    // Вычисляем общий размер файлов
     for (const file of files) {
       const filePath = `files/${file}`;
-      const fileData = loadFile(filePath);
-      if (fileData) {
-        console.log(`Загружен файл: ${filePath}`);
-      }
+      const data = fs.readFileSync(filePath, "utf8");
+      totalSize += Buffer.byteLength(data, "utf8");
+    }
+
+    // Процент загрузки
+    let loadedSize = 0;
+
+    for (const file of files) {
+      const filePath = `files/${file}`;
+      loadedSize += loadFile(filePath, totalSize);
+      const percent = (loadedSize / totalSize) * 100; // Процент загруженного от общего размера
+      updateProgressBar(percent);
+      console.log(`Загружен файл: ${filePath}`);
     }
   } catch (err) {
     console.error("Произошла ошибка при чтении файлов:", err);
   }
+}
+
+// Функция для обновления шкалы прогресса
+function updateProgressBar(percent) {
+  const progressBarLength = 50;
+  const completeLength = Math.floor((percent / 100) * progressBarLength);
+
+  let progressBar = "[";
+  for (let i = 0; i < progressBarLength; i++) {
+    if (i < completeLength) {
+      progressBar += "=";
+    } else {
+      progressBar += " ";
+    }
+  }
+  progressBar += "]";
+
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(progressBar + ` ${percent.toFixed(2)}%`);
 }
 
 loadFiles();
