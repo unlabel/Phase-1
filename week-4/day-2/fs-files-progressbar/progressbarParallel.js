fbaea38505d7f9f9e1aed90efff17f4802c6cabf
Denis Ignatenko <luuffyakka@yandex.ru>
2023-10-31 10:15:14 +0300

    Sync

--- week-4/day-2/fs-files-progressbar/progressbarParallel.js
+++ week-4/day-2/fs-files-progressbar/progressbarParallel.js
@@ -1,10 +1,20 @@
-const fs = require('fs');
+const fs = require("fs");
+const waitSync = require("wait-sync");
 // Раскомментируй только для релиза 4.
 // const runInteractiveConsole = require('./interactive');
 // runInteractiveConsole();
 
 // Асинхронная загрузка одного файла
-function loadFile(filePath, callback) {}
+function loadFile(filePath, callback) {
+  fs.readdirSync(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Произошла ошибка при чтении файла:", err);
+      return;
+    }
+    const dataArray = callback(data.split(" "));
+    const result = dataArray.join(" ");
+  });
+}
 
 // Асинхронная параллельная загрузка файлов
 function loadFiles() {}
--- week-4/day-2/fs-files-progressbar/progressbarSync.js
+++ week-4/day-2/fs-files-progressbar/progressbarSync.js
@@ -1,12 +1,44 @@
-const fs = require('fs');
+const fs = require("fs");
 // Раскомментируй только для релиза 4.
 // const runInteractiveConsole = require('./interactive');
 // runInteractiveConsole();
 
 // Синхронная загрузка одного файла
-function loadFile(filePath) {}
+function loadFile(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, "utf8");
+    const fileSize = Buffer.byteLength(data, "utf8");
+    const delay = Math.floor(fileSize / 250);
+    sleep(delay); // Имитация задержки загрузки
+    return data;
+  } catch (err) {
+    console.error("Произошла ошибка при чтении файла:", err);
+    return null;
+  }
+}
+
+// Функция для имитации задержки
+function sleep(ms) {
+  const start = Date.now();
+  while (Date.now() - start < ms) {
+    // Пустой цикл для задержки
+  }
+}
 
 // Синхронная загрузка файлов
-function loadFiles() {}
+function loadFiles() {
+  try {
+    const files = fs.readdirSync("files");
+    for (const file of files) {
+      const filePath = `files/${file}`;
+      const fileData = loadFile(filePath);
+      if (fileData) {
+        console.log(`Загружен файл: ${filePath}`);
+      }
+    }
+  } catch (err) {
+    console.error("Произошла ошибка при чтении файлов:", err);
+  }
+}
 
 loadFiles();
