2985a5dd16840287cae86a7bf22d5261d512550d
Denis Ignatenko <luuffyakka@yandex.ru>
2023-11-07 15:23:34 +0300

    Release 0-4

--- week-5/day-1/db-design-1nf-books/sql/migration.sql
+++ week-5/day-1/db-design-1nf-books/sql/migration.sql
@@ -1,1 +1,11 @@
 -- Здесь пиши код для создания и удаления таблиц(ы).
+CREATE TABLE IF NOT EXISTS books (
+    book_id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    isbn BIGINT NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    genres VARCHAR(255) NOT NULL,
+    amount INTEGER NOT NULL
+);
+
+DROP TABLE IF EXISTS books;
--- week-5/day-1/db-design-1nf-books/sql/queries.sql
+++ week-5/day-1/db-design-1nf-books/sql/queries.sql
@@ -1,1 +1,21 @@
 -- Здесь пиши код для выборки данных из таблиц(ы).
+SELECT title FROM books
+WHERE amount > 0;
+
+SELECT title FROM books
+WHERE amount = 0;
+
+SELECT title FROM books
+WHERE amount >= 5;
+
+SELECT title, amount FROM books
+WHERE amount BETWEEN 6 and 9;
+
+SELECT title, amount FROM books
+WHERE amount > 0
+ORDER BY amount DESC;
+
+SELECT title, amount FROM books
+WHERE amount > 0
+ORDER BY amount LIMIT 5;
+
